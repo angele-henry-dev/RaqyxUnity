847da77dab770c4c07dd7c6761fddd6dcbdcb6d9
Angèle Henry <angele.henry@outlook.com>
2024-07-25 17:50:24 +0200

    Trying something else

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -13,32 +13,20 @@ public class Player : MonoBehaviour
 
     [Header("Config")]
     [SerializeField]
-    private float moveSpeed = 1.5f;
+    private float speed = 1.5f;
     [SerializeField]
     private Vector2 startPosition;
 
-    [SerializeField]
     public bool isTerritoryInProgress = false;
 
-    [SerializeField]
-    private Direction direction = Direction.RIGHT;
+    public Vector2 direction { get; private set; }
+    public Vector2 nextDirection { get; private set; }
 
-    private Vector2[] territoryPoints;
     private float playerDecay;
     private const string axisHorizontal = "Horizontal";
     private const string axisVertical = "Vertical";
     private const string tagWall = "Wall";
     private const string tagWallOutside = "WallOutside";
-
-    private readonly float pointThreshold = 0.1f;
-
-    enum Direction
-    {
-        UP,
-        DOWN,
-        RIGHT,
-        LEFT
-    }
 
     void Start()
     {
@@ -50,111 +38,156 @@ public class Player : MonoBehaviour
         EventManager.StartListening(EventManager.Event.onStartGame, ResetPosition);
     }
 
-    void Update()
-    {
-        float[] movements = ProcessInput();
-        if (movements != null)
-        {
-            MoveManually(movements);
-        }
-    }
-
-    void FixedUpdate()
-    {
-        MoveAuto();
-    }
-
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        Debug.Log("Collision!");
-        if (collision.gameObject.CompareTag(tagWall) && isTerritoryInProgress)
-        {
-            ChangeDirection(GetNextMove());
-        } else if (collision.gameObject.CompareTag(tagWallOutside) && !isTerritoryInProgress)
-        {
-            ChangeDirection(GetNextMove());
-        }
-    }
-
     void OnDestroy()
     {
         EventManager.StopListening(EventManager.Event.onReset, ResetPosition);
     }
 
-    // Movements
-
-    private void MoveAuto()
+    void FixedUpdate()
     {
-        Vector2 newDirection = transform.up;
-        rb2d.velocity = newDirection * moveSpeed;
-
-        /*if (!isTerritoryInProgress)
-        {
-            Vector2? nextPoint = TriggerPolygonPoint();
-            if (nextPoint != null)
-            {
-                transform.position = nextPoint.Value;
-                ChangeDirection(GetNextMove());
-            }
-        }*/
+        Vector2 position = rb2d.position;
+        Vector2 translation = speed * Time.fixedDeltaTime * direction;
+        rb2d.MovePosition(position + translation);
     }
 
-    private void MoveManually(float[] movements)
+    void Update()
     {
-        ChangeDirection(movements);
-        isTerritoryInProgress = true;
+        if (nextDirection != Vector2.zero)
+        {
+            SetDirection(nextDirection);
+        }
+
+        if (Input.GetAxis(axisVertical) > 0)
+        {
+            SetDirection(Vector2.up);
+        }
+        else if (Input.GetAxis(axisVertical) < 0)
+        {
+            SetDirection(Vector2.down);
+        }
+        else if (Input.GetAxis(axisHorizontal) < 0)
+        {
+            SetDirection(Vector2.left);
+        }
+        else if (Input.GetAxis(axisHorizontal) > 0)
+        {
+            SetDirection(Vector2.right);
+        }
+
+        AdjustSpriteRotation();
+    }
+
+    public void SetDirection(Vector2 direction, bool forced = false)
+    {
+        //if (forced || Valid(direction))
+        //{
+            this.direction = direction;
+            nextDirection = Vector2.zero;
+        //}
+    }
+
+    /*void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collision!");
+        if (collision.gameObject.CompareTag(tagWall) && isTerritoryInProgress)
+        {
+            Debug.Log("Collision wall!");
+            ChangeDirection(GetNextMove());
+        } else if (collision.gameObject.CompareTag(tagWallOutside) && !isTerritoryInProgress)
+        {
+            Debug.Log("Collision outside wall!");
+            ChangeDirection(GetNextMove());
+            Debug.Log($"Go {direction}");
+        }
+    }*/
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collision");
+        if (collision.gameObject.CompareTag(tagWallOutside))
+        {
+            Debug.Log("Collision with outside wall");
+            if (direction == Vector2.right)
+            {
+                nextDirection = new(x: 0, y: -1);
+
+            }
+            else if (direction == Vector2.left)
+            {
+                nextDirection = new(x: 0, y: 1);
+            }
+
+            else if (direction == Vector2.down)
+            {
+                nextDirection = new(x: -1, y: 0);
+            }
+
+            else if (direction == Vector2.up)
+            {
+                nextDirection = new(x: 1, y: 0);
+            }
+            else
+            {
+                nextDirection = Vector2.zero;
+            }
+        }
+    }
+
+    /*public bool Occupied(Vector2 direction)
+    {
+        // If no collider is hit then there is no obstacle in that direction
+        RaycastHit2D hit = Physics2D.BoxCast(transform.position, Vector2.one * 0.75f, 0f, direction, 1.5f);
+        return hit.collider != null;
+    }*/
+
+    bool Valid(Vector2 dir)
+    {
+        // TODO Adapt it to ignore inside walls
+        Vector2 pos = transform.position;
+        RaycastHit2D hit = Physics2D.Linecast(pos + dir, pos);
+        Debug.Log($"{hit.collider.tag}");
+        return hit.collider.CompareTag(tagWallOutside);
+    }
+
+    /*private float[] ProcessInput()
+    {
+        float[] movements = { Input.GetAxis(axisHorizontal), Input.GetAxis(axisVertical) };
+
+        if (movements[0] == 0 && movements[1] == 0)
+        {
+            return null;
+        }
+        else if (Mathf.Abs(movements[0]) > Mathf.Abs(movements[1]))
+        {
+            movements[0] = movements[0] < 0 ? -1 : 1;
+            movements[1] = 0;
+        }
+        else
+        {
+            movements[0] = 0;
+            movements[1] = movements[1] < 0 ? -1 : 1;
+        }
+        return movements;
     }
 
     private void ChangeDirection(float[] movements)
     {
         Vector2 velo = rb2d.velocity;
-        velo.x = moveSpeed * movements[0];
-        velo.y = moveSpeed * movements[1];
+        velo.x = speed * movements[0];
+        velo.y = speed * movements[1];
         rb2d.velocity = velo;
         AdjustSpriteRotation();
-    }
-
-    private float[] GetNextMove()
-    {
-        float[] movements = direction switch
-        {
-            Direction.RIGHT => new[] { 0f, -1f },
-            Direction.LEFT => new[] { 0f, 1f },
-            Direction.DOWN => new[] { -1f, 0f },
-            _ => new[] { 1f, 0f },
-        };
-        return movements;
-    }
-
-    // UI
+    }*/
 
     private void AdjustSpriteRotation()
     {
-        float rotation = 0f;
-        direction = Direction.UP;
-
-        if (rb2d.velocity.x < 0f)
-        {
-            rotation = 90f;
-            direction = Direction.LEFT;
-        } else if (rb2d.velocity.x > 0f)
-        {
-            rotation = 270f;
-            direction = Direction.RIGHT;
-        }
-        else if (rb2d.velocity.y < 0f)
-        {
-            rotation = 180f;
-            direction = Direction.DOWN;
-        }
-        transform.eulerAngles = new Vector3(0, 0, rotation);
-
-        /*float angle = Mathf.Atan2(rb2d.velocity.y, rb2d.velocity.x) * Mathf.Rad2Deg;
-        rb2d.rotation = angle - 90f;*/
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        rb2d.rotation = angle - 90f;
     }
 
     private void ResetPosition(Dictionary<string, object> message = null)
     {
+        nextDirection = Vector2.zero;
         transform.position = startPosition;
 
         /*Vector2 direction = startPosition - rb2d.position;
@@ -177,32 +210,79 @@ public class Player : MonoBehaviour
         rb2d.velocity = newDirection.normalized * moveSpeed;*/
     }
 
-    // Trigger
-
-    private float[] ProcessInput()
+    /*void Update()
     {
-        float[] movements = { Input.GetAxis(axisHorizontal), Input.GetAxis(axisVertical) };
-
-        if (movements[0] == 0 && movements[1] == 0)
+        float[] movements = ProcessInput();
+        if (movements != null)
         {
-            return null;
+            MoveManually(movements);
         }
-        else if (Mathf.Abs(movements[0]) > Mathf.Abs(movements[1]))
-        {
-            movements[1] = 0;
-        }
-        else
-        {
-            movements[0] = 0;
-        }
-        return movements;
     }
 
-    private void BackOnPolygon()
+    void FixedUpdate()
+    {
+        MoveAuto();
+    }*/
+
+    /*void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collision!");
+        if (collision.gameObject.CompareTag(tagWall) && isTerritoryInProgress)
+        {
+            Debug.Log("Collision wall!");
+            ChangeDirection(GetNextMove());
+        } else if (collision.gameObject.CompareTag(tagWallOutside) && !isTerritoryInProgress)
+        {
+            Debug.Log("Collision outside wall!");
+            ChangeDirection(GetNextMove());
+            Debug.Log($"Go {direction}");
+        }
+    }*/
+
+    // Movements
+
+    /*private void MoveAuto()
+    {
+        Vector2 newDirection = transform.up;
+        rb2d.velocity = newDirection * moveSpeed;
+
+        /*if (!isTerritoryInProgress)
+        {
+            Vector2? nextPoint = TriggerPolygonPoint();
+            if (nextPoint != null)
+            {
+                transform.position = nextPoint.Value;
+                ChangeDirection(GetNextMove());
+            }
+        }
+    }
+
+    private void MoveManually(float[] movements)
+    {
+        ChangeDirection(movements);
+        isTerritoryInProgress = true;
+    }
+
+    private float[] GetNextMove()
+    {
+        float[] movements = direction switch
+        {
+            Direction.RIGHT => new[] { 0f, -1f },
+            Direction.LEFT => new[] { 0f, 1f },
+            Direction.DOWN => new[] { -1f, 0f },
+            _ => new[] { 1f, 0f },
+        };
+        return movements;
+    }*/
+
+    // UI
+
+    // Trigger
+
+    /*private void BackOnPolygon()
     {
         isTerritoryInProgress = false;
         // ContactPoint2D contact = collision.GetContact(0);
-        // TODO
 
         switch (direction) {
             case Direction.RIGHT:
@@ -225,12 +305,12 @@ public class Player : MonoBehaviour
             Direction.LEFT => new(x: (transform.position.x), y: transform.position.y, z: transform.position.z),
             Direction.DOWN => new(x: transform.position.x, y: (transform.position.y), z: transform.position.z),
             _ => new(x: transform.position.x, y: (transform.position.y), z: transform.position.z),
-        };*/
+        };
 
         ChangeDirection(GetNextMove());
-    }
+    }*/
 
-    private Vector2? TriggerPolygonPoint()
+    /*private Vector2? TriggerPolygonPoint()
     {
         Vector2 startingFrom = transform.position;
         Vector2? targetPoint = GetNextPoint();
@@ -287,5 +367,5 @@ public class Player : MonoBehaviour
     private bool IsEqualTo(float a, float b)
     {
         return System.Math.Abs(a - b) < float.Epsilon;
-    }
+    }*/
 }
